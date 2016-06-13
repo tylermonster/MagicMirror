ae397d72eb29ce60e79f8c4d068725247ff89526
tyler nguyen <tyler nguyen>
2016-06-13 15:49:09 -0500

    no message

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <meta http-equiv="expires" content="Tue, 01 Jan 1980 11:00:00 GMT">
 <meta http-equiv="pragma" content="no-cache">
 
-<meta http-equiv="refresh" content="480">
+<meta http-equiv="refresh" content="30">
 	<link rel="icon" href="data:;base64,iVBORw0KGgo=">
 </head>
 <body>
